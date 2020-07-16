acce8e748fba8a6b3d1113aa96954c6934b6f974
mostafa-ihab <jehad403448@gmail.com>
2020-07-16 20:21:29 +0300

    add job models

--- Frontend-Template/jobboard2/outline.md
+++ Frontend-Template/jobboard2/outline.md
@@ -31,3 +31,4 @@ contact
 home
 
 login
+
--- job/models.py
+++ job/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+job_type ={
+  (  'Full Time','Full Time'),
+    ('Part Time','Part Time')
+} 
+
+class Job(models.Model):
+    title = models.CharField(max_length=70)
+    #location
+    job_type = models.TextField(max_length=50,choices=job_type) 
+    description = models.TextField(max_length=1000)
+    publish_at = models.DateTimeField(auto_now=True )
+    Vacancy = models.IntegerField(default=1)
+    salary = models.IntegerField(default=0)
+    experiance = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.title    
