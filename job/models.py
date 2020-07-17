2cb094372a5ca5f63271b5a4f4684eea525f4471
mostafa-ihab <jehad403448@gmail.com>
2020-07-18 02:11:21 +0300

    we arrive to 10 viedio

--- job/models.py
+++ job/models.py
@@ -9,12 +9,21 @@ job_type ={
 class Job(models.Model):
     title = models.CharField(max_length=70)
     #location
-    job_type = models.TextField(max_length=50,choices=job_type) 
-    description = models.TextField(max_length=1000)
-    publish_at = models.DateTimeField(auto_now=True )
-    Vacancy = models.IntegerField(default=1)
-    salary = models.IntegerField(default=0)
-    experiance = models.IntegerField(default=1)
-
+    job_type         = models.TextField(max_length=50,choices=job_type) 
+    description      = models.TextField(max_length=1000)
+    publish_at       = models.DateTimeField(auto_now=True )
+    Vacancy          = models.IntegerField(default=1)
+    salary           = models.IntegerField(default=0)
+    experiance       = models.IntegerField(default=1)
+    category         = models.ForeignKey('Category' , on_delete=models.CASCADE , null=True )
+    image            =models.ImageField(upload_to ='jobs/')
     def __str__(self):
-        return self.title    
+        return self.title
+
+
+class Category(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=25)
+   
+    def __str__(self):
+        return self.name    
--- job/views.py
+++ job/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from .models import Job
 # Create your views here.
+
+def job_list(request):
+    jobs = Job.objects.all()
+    context = {'jobs': jobs}
+    return render(request , 'job/jobs.html', context)
+
+
+
+
+def job_details(request , id ):
+    job = Job.objects.get( id =id )
+    context = {
+        'jobs' : job
+    }
+    return render(request , 'job/job_details.html' , context )
